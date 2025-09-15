197dbc8f919044f7a774e604cddce6e6e9015448
farah <farahx.x@hotmail.com>
2025-09-15 16:06:29 +0100

    test for CICD webhook

--- src/test/java/com/farah/filmschool/services/CourseServiceTests.java
+++ src/test/java/com/farah/filmschool/services/CourseServiceTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.List;
